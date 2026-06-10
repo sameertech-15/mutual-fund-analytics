e27a3df9e2f1399f6fc54762df8ea63ddf02908b
sameertech-15 <dsameer0530@gmail.com>
2026-06-10 12:28:03 +0530

    Updated fund recommender using Sharpe ratio

--- scripts/recommender.py
+++ scripts/recommender.py
@@ -1,6 +1,16 @@
+import os
 import pandas as pd
 
-df = pd.read_csv("../data/processed/07_scheme_performance_cleaned.csv")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+file_path = os.path.join(
+    BASE_DIR,
+    "data",
+    "processed",
+    "07_scheme_performance_cleaned.csv"
+)
+
+df = pd.read_csv(file_path)
 
 def recommend_funds(risk_grade="Low", top_n=3):
 
@@ -12,21 +22,20 @@ def recommend_funds(risk_grade="Low", top_n=3):
     recommendations = (
         filtered
         .sort_values(
-            by="sharpe_ratio"
+            by="sharpe_ratio",
             ascending=False
         )
         .head(top_n)
     )
 
     return recommendations[
-    [
-        "amfi_code",
-        "scheme_name",
-        "sharpe_ratio",
-        "risk_grade"
+        [
+            "amfi_code",
+            "scheme_name",
+            "sharpe_ratio",
+            "risk_grade"
+        ]
     ]
-]
 
 if __name__ == "__main__":
-
     print(recommend_funds("Low"))
